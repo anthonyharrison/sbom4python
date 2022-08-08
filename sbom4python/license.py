339d9544371ba857577e16feba4ab1ba097bdd38
Anthony Harrison <anthony.p.harrison@gmail.com>
2022-08-08 21:59:39 +0100

    Handle Apache 2.0 licence synoymns

--- sbom4python/license.py
+++ sbom4python/license.py
@@ -7,12 +7,18 @@ import os
 
 
 class LicenseScanner:
+
+    APACHE_SYNOYMNS = ["Apache Software License", "Apache License, Version 2.0", "Apache 2.0", "Apache 2"]
+
     def __init__(self):
         # Load licenses
         license_dir, filename = os.path.split(__file__)
         license_path = os.path.join(license_dir, "license_data", "spdx_licenses.json")
         licfile = open(license_path)
         self.licenses = json.load(licfile)
+
+    def check_synoymn(self, license, synoymns, value):
+        return value if license in synoymns else None
 
     def find_license(self, license):
         # Search list of licenses to find match
@@ -24,4 +30,5 @@ class LicenseScanner:
                 return lic["licenseId"]
             elif lic["name"].lower() == license.lower():
                 return lic["licenseId"]
-        return default_license
+        license_id = self.check_synoymn(license, self.APACHE_SYNOYMNS, "Apache-2.0")
+        return license_id if license_id is not None else default_license
