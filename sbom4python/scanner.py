5fb9f8bf3f5ffa5f77d68cbd652651b01a469760
anthonyharrison <anthony.p.harrison@gmail.com>
2024-11-25 22:18:33 +0000

    feat: use importlib to extract metadata (fixes #17)

--- sbom4python/scanner.py
+++ sbom4python/scanner.py
@@ -1,13 +1,22 @@
 # Copyright (C) 2023 Anthony Harrison
 # SPDX-License-Identifier: Apache-2.0
 
+import ast
+# import importlib.metadata
 import pathlib
 import platform
 import re
+import string
 import subprocess
+import sys
 import unicodedata
-import ast
 
+if sys.version_info >= (3, 10):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+import pkg_resources
 from lib4package.metadata import Metadata
 from lib4sbom.data.document import SBOMDocument
 from lib4sbom.data.package import SBOMPackage
@@ -22,7 +31,13 @@ class SBOMScanner:
     """
 
     def __init__(
-        self, debug, include_file=False, exclude_license=False, lifecycle="build", include_service=False
+        self,
+        debug,
+        include_file=False,
+        exclude_license=False,
+        lifecycle="build",
+        include_service=False,
+        use_pip=False,
     ):
         self.record = []
         self.debug = debug
@@ -42,6 +57,7 @@ class SBOMScanner:
         self.python_version = platform.python_version()
         self.set_lifecycle(lifecycle)
         self.metadata = {}
+        self.use_pip = use_pip
 
     def set_parent(self, module):
         self.parent = f"Python-{module}"
@@ -75,7 +91,8 @@ class SBOMScanner:
         # Use RFC-5322 compliant regex (https://regex101.com/library/6EL6YF)
         emails = re.findall(
             r"((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))",
-            supplier_info,re.IGNORECASE
+            supplier_info,
+            re.IGNORECASE,
         )
         supplier = " ".join(n for n in names)
         if include_email and len(emails) > 0:
@@ -83,22 +100,25 @@ class SBOMScanner:
             supplier = supplier + "(" + emails[-1] + ")"
         return re.sub(" +", " ", supplier.strip())
 
-    def _create_package(self, package, version, parent="-", requirements = None):
+    def _create_package(self, package, version, parent="-", requirements=None):
         self.sbom_package.initialise()
         offline = False
         try:
             self.package_metadata.get_package(package)
-        except:
+        except Exception as ex:
             offline = True
             if self.debug:
-                print(f"[ERROR] Unable to retrieve metadata for {package}")
+                print(f"[ERROR] Unable to retrieve metadata for {package} - {ex}")
         self.sbom_package.set_name(package)
         self.sbom_package.set_property("language", "Python")
         self.sbom_package.set_property("python_version", self.python_version)
         if not offline:
             # External metadata may lag releases
             if self.package_metadata.get_latest_version() == version:
-                self.sbom_package.set_property("package_release_date", self.package_metadata.get_latest_release_time())
+                self.sbom_package.set_property(
+                    "package_release_date",
+                    self.package_metadata.get_latest_release_time(),
+                )
         self.sbom_package.set_version(version)
         if requirements is not None:
             self.sbom_package.set_evidence(requirements)
@@ -161,9 +181,72 @@ class SBOMScanner:
             self.sbom_package.set_homepage(home_page)
         if summary != "":
             self.sbom_package.set_summary(summary)
-        self.sbom_package.set_downloadlocation(
-            f"https://pypi.org/project/{package}/{version}/#files"
-        )
+        if self.metadata.get("Project-URL") is not None:
+            # Extra references
+            # Normalisation of labels
+            chars_to_remove = string.punctuation + string.whitespace
+            removal_map = str.maketrans("", "", chars_to_remove)
+            # Various synonyms of project URLs
+            categories = {
+                "docs": "documentation",
+                "source": "vcs",
+                "repository": "vcs",
+                "sourcecode": "vcs",
+                "github": "vcs",
+                "githubrepo": "vcs",
+                "gitlab": "vcs",
+                "bitbucket": "vcs",
+                "git": "vcs",
+                "sourceforge": "vcs",
+                "svn": "vcs",
+                "code": "vcs",
+                "changelog": "log",
+                "changes": "log",
+                "docschangelog": "log",
+                "whatsnew": "log",
+                "issues": "issue-tracker",
+                "bug": "issue-tracker",
+                "bugs": "issue-tracker",
+                "bugreports": "issue-tracker",
+                "bugtracker": "issue-tracker",
+                "issuetracker": "issue-tracker",
+                "tracker": "issue-tracker",
+                "githubissues": "issue-tracker",
+                "mailinglist": "mailing-list",
+                "mailinglists": "mailing-list",
+                "sourcedistribution": "source-distribution",
+                "ci": "build-system",
+                "cigithub": "build-system",
+                "cigithubactions": "build-system",
+                "buildsystem": "build-systen",
+                "releasenotes": "release-notes",
+                "releasen": "release-notes",
+                "twitter": "social",
+                "discord": "social",
+                "home": "home-page",
+                "homepage": "home-page",
+                "home page": "home-page",
+            }
+            for ref in self.metadata.get("Project-URL"):
+                category = ref.split(", ")[0].translate(removal_map).lower()
+                locator = ref.split(", ")[1]
+                # See if synonymn
+                if categories.get(category) is not None:
+                    if self.debug:
+                        print(
+                            f"Updating category from {category} to {categories[category]}"
+                        )
+                    category = categories[category]
+                if category == "home-page":
+                    self.sbom_package.set_homepage(locator)
+                else:
+                    self.sbom_package.set_externalreference("OTHER", category, locator)
+        if self.metadata.get("Download-URL") is None:
+            self.sbom_package.set_downloadlocation(
+                f"https://pypi.org/project/{package}/{version}/#files"
+            )
+        else:
+            self.sbom_package.set_downloadlocation(self.metadata.get("Download-URL"))
         # External references
         self.sbom_package.set_purl(f"pkg:pypi/{package}@{version}")
         if len(supplier) > 1:
@@ -175,6 +258,8 @@ class SBOMScanner:
         checksum = self.package_metadata.get_checksum(version=version)
         if checksum is not None:
             self.sbom_package.set_checksum("SHA1", checksum)
+        # Copyright
+        self.sbom_package.set_copyrighttext("NOASSERTION")
         # Store package data
         self.sbom_packages[
             (
@@ -203,7 +288,7 @@ class SBOMScanner:
             A list of potential external service interactions.
         """
         potential_external_services = []
-        modules=["requests", "urllib", "httplib2"]
+        modules = ["requests", "urllib", "httplib2"]
         potential_endpoint = []
         try:
             with open(filename, "r", errors="replace") as f:
@@ -214,16 +299,25 @@ class SBOMScanner:
                 if isinstance(node, ast.Attribute):
                     # Check for function calls on http libraries like requests or urllib
                     if (
-                            isinstance(node.value, ast.Name)
-                            and (node.value.id in modules)
-                            and node.attr in ["get", "post", "put", "delete"]
+                        isinstance(node.value, ast.Name)
+                        and (node.value.id in modules)
+                        and node.attr in ["get", "post", "put", "delete"]
                     ):
-                        if [node.value.id, node.attr] not in potential_external_services:
-                            potential_external_services.append([node.value.id, node.attr])
+                        if [
+                            node.value.id,
+                            node.attr,
+                        ] not in potential_external_services:
+                            potential_external_services.append(
+                                [node.value.id, node.attr]
+                            )
                 elif isinstance(node, ast.Constant):
                     if node.value is not None:
                         constant = str(node.value)
-                        if constant.startswith("http") and "//" in constant and len(constant) > 8:
+                        if (
+                            constant.startswith("http")
+                            and "//" in constant
+                            and len(constant) > 8
+                        ):
                             # print (filename, constant)
                             potential_endpoint.append(constant)
         except FileNotFoundError:
@@ -233,43 +327,116 @@ class SBOMScanner:
             pass
         if len(potential_external_services) > 0 and len(potential_endpoint) > 0:
             if self.debug:
-                print (f"Potential endpoint in {filename}")
+                print(f"Potential endpoint in {filename}")
                 for i in potential_endpoint:
                     print(i)
                 for i in potential_external_services:
-                    print (i)
+                    print(i)
 
             return potential_endpoint
         else:
             return []
 
-    def process_module(self, module, parent="-"):
-        if self.debug:
-            print(f"Process Module {module}")
-        out = self.run_program(f"pip show {module}")
-        # If: module not found, no metadata returned
-        if len(out) > 0:
-            self.metadata = {}
+    def _getpackage_metadata(self, module):
+        metadata = {}
+        if self.use_pip:
+            out = self.run_program(f"pip show {module}")
             for line in out:
                 entry = line.split(":")
-                # If: this line contain an non empty entry delimited by ':'
+                # If: this line contain an non-empty entry delimited by ':'
                 if (len(entry) == 2) and (entry[1] and not (entry[1].isspace())):
-                    # then: store all data after keyword
-                    self.metadata[entry[0]] = (
+                    # Store all data after keyword
+                    metadata[entry[0]] = (
                         line.split(f"{entry[0]}:", 1)[1].strip().rstrip("\n")
                     )
                 elif len(entry) > 2:
                     # Likely to include URL
-                    self.metadata[entry[0]] = (
+                    metadata[entry[0]] = (
                         line.split(f"{entry[0]}:", 1)[1].strip().rstrip("\n")
                     )
+        else:
+            try:
+                package_data = importlib_metadata.metadata(module)
+            except importlib_metadata.PackageNotFoundError:
+                package_data = None
+            if package_data is None:
+                if self.debug:
+                    print(f"Unable to retrieve metadata for {module}")
+                return metadata
+            package_metadata = dict(package_data)
+            # Store subset of metadata (same as pip show <module>)
+            for attribute in [
+                "Name",
+                "Version",
+                "Summary",
+                "Home-page",
+                "Author",
+                "Author-email",
+                "License",
+                "Download-URL",
+            ]:
+                if package_metadata.get(attribute) is not None:
+                    metadata[attribute] = package_metadata[attribute]
+            # License-Expresssion is preferred to License
+            if package_metadata.get("License_Expression"):
+                metadata["License"] = package_metadata["License_Expression"]
+            # Project-URL (multiple)
+            if package_metadata.get("Project-URL"):
+                metadata["Project-URL"] = package_data.get_all("Project-URL")
+            # Requires-Dist (multiple)
+            if package_metadata.get("Requires-Dist"):
+                requires = package_data.get_all("Requires-Dist")
+            else:
+                requires = None
+
+            def extract_package_name(requirement_string):
+                for i, char in enumerate(requirement_string):
+                    # Ignore optional dependencies
+                    if "extra" in requirement_string:
+                        return ""
+                    # Paqckage names only contain alphanumeric characters and -
+                    if not char.isalnum() and char != "-":
+                        return requirement_string[:i]
+                return requirement_string
+
+            def extract_package_names(requirements_list):
+                return [extract_package_name(req) for req in requirements_list]
+
+            if requires is not None:
+                # Find dependent packages
+                if self.debug:
+                    print(f"Dependencies for {module} - {requires}")
+
+                package_names = extract_package_names(requires)
+
+                package_dependendents = ""
+                for name in package_names:
+                    # Ignore extra packages
+                    if len(name) > 0:
+                        package_dependendents = (
+                            package_dependendents + name.split(" ")[0] + ", "
+                        )
+                # Remove extra punctuation
+                metadata["Requires"] = package_dependendents[:-2]
+            else:
+                metadata["Requires"] = ""
+        if self.debug:
+            print(metadata)
+        return metadata
+
+    def process_module(self, module, parent="-"):
+        if self.debug:
+            print(f"Process Module {module}")
+        self.metadata = self._getpackage_metadata(module.strip())
+        # If module not found, no metadata returned
+        if len(self.metadata) > 0:
             package = self.get("Name").lower().replace("_", "-")
             version = self.get("Version")
             if (package, version) in self.sbom_packages:
                 if self.debug:
                     print(f"Already processed {package} {version}")
                 # Prevent metadata being reprocessed
-                out = ""
+                self.metadata = {}
             else:
                 if self.debug:
                     print(f"Metadata for {module}\n{self.metadata}")
@@ -280,14 +447,14 @@ class SBOMScanner:
                 directory_location = f'{self.get("Location")}/{package}'
                 file_dir = pathlib.Path(directory_location)
                 if self.debug:
-                    print (f"Directory for {package}: {file_dir}")
+                    print(f"Directory for {package}: {file_dir}")
                 if file_dir.exists():
                     filtered = [x for x in file_dir.glob("**/*")]
                 else:
                     # Module is only a single file
                     filtered = [pathlib.Path(f'{self.get("Location")}/{package}')]
                 if self.debug:
-                    print (f"Filenames: {filtered}")
+                    print(f"Filenames: {filtered}")
                 for entry in filtered:
                     # Ignore compiled code
                     if str(entry).endswith(".pyc"):
@@ -296,8 +463,8 @@ class SBOMScanner:
                         print(f"Analyse file in {entry}")
                     if self.include_service:
                         external_services = self.analyze_code(entry)
-                    # if len(external_services) > 0:
-                    #     print (f"External services in {entry}")
+                        if len(external_services) > 0:
+                            print(f"External services in {entry}")
 
                     if self.file_scanner.scan_file(entry):
                         self.sbom_files[
@@ -318,7 +485,7 @@ class SBOMScanner:
                         )
         elif self.debug:
             print(f"Module {module} not found")
-        return len(out) > 0
+        return len(self.metadata) > 0
 
     def get(self, attribute):
         return self.metadata.get(attribute, "").lstrip()
@@ -353,19 +520,27 @@ class SBOMScanner:
         if self.process_module(module_name):
             self.analyze(self.get("Name"), self.get("Requires"))
 
+    def _get_installed_modules(self):
+        modules = []
+        if self.use_pip:
+            out = self.run_program("pip list")
+            if len(out) > 0:
+                # Ignore headers in output stream
+                for m in out[2:]:
+                    modules.append(m.split(" ")[0])
+        else:
+            installed_packages = pkg_resources.working_set
+            modules = sorted(["%s" % (i.key) for i in installed_packages])
+        if self.debug:
+            print(modules)
+        return modules
+
     def process_system(self):
-        out = self.run_program("pip list")
-        if len(out) > 0:
-            modules = []
-            # Ignore headers in output stream
-            for m in out[2:]:
-                modules.append(m.split(" ")[0])
-            if self.debug:
-                print(modules)
-            self.set_parent("system")
-            for module_name in modules:
-                if self.process_module(module_name):
-                    self.analyze(self.get("Name"), self.get("Requires"))
+        modules = self._get_installed_modules()
+        self.set_parent("system")
+        for module_name in modules:
+            if self.process_module(module_name):
+                self.analyze(self.get("Name"), self.get("Requires"))
 
     def process_requirements(self, filename):
         if len(filename) > 0:
@@ -386,5 +561,5 @@ class SBOMScanner:
                         version = component[1]
                         if self.debug:
                             print(f"Processing {package} version {version}")
-                        self._create_package(package, version, requirements = filename)
+                        self._create_package(package, version, requirements=filename)
                         self._create_relationship(package)
